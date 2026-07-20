60f012ee04e411769632247c216ed1642601c378
pinwheeeel <152241032+pinwheeeel@users.noreply.github.com>
2026-07-20 18:31:44 -0400

    engine: add tests
    test all the basic things such as creating a new engine object, testing public api, making sure fields are all correct etc. testing moves is more complex and will be implemented later

---
+++ engine/engine_test.go
@@ -0,0 +1,95 @@
+package engine
+
+import (
+	"testing"
+)
+
+func TestEngineConstruction(t *testing.T) {
+	e := NewWithSeed(0)
+
+	if e == nil {
+		t.Errorf("engine is nil")
+	}
+	if e.grid == nil {
+		t.Errorf("grid is nil")
+	}
+	if e.score != 0 {
+		t.Errorf("score is not 0")
+	}
+	if e.bestTile != 2 {
+		t.Errorf("bestTile is not 2")
+	}
+	if e.status != Playing {
+		t.Errorf("status is not Playing")
+	}
+}
+
+func TestGridAt(t *testing.T) {
+	e := NewWithSeed(0)
+
+	for i := range 4 {
+		for j := range 4 {
+			gridAt, err := e.GridAt(i, j)
+			if err != nil {
+				t.Errorf("GridAt(%d, %d) returned error: %v", i, j, err)
+			}
+
+			if e.grid[i][j] != gridAt {
+				t.Errorf("grid[%d][%d] != GridAt(%d, %d)", i, j, i, j)
+			}
+		}
+	}
+}
+
+func TestGridOutOfBounds(t *testing.T) {
+	e := NewWithSeed(0)
+
+	a := [][2]int{{-1, 0}, {0, -1}, {4, 0}, {0, 4}}
+
+	for _, coords := range a {
+		row, col := coords[0], coords[1]
+		_, err := e.GridAt(row, col)
+		if err == nil {
+			t.Errorf("GridAt(%d, %d) did not return error", row, col)
+		}
+	}
+}
+
+func TestNewGame(t *testing.T) {
+	e := NewWithSeed(0)
+
+	startingTiles := 0
+	for i := range 4 {
+		for j := range 4 {
+			tileValue := e.grid[i][j]
+			if tileValue != 0 {
+				startingTiles++
+
+				if tileValue != 2 {
+					t.Errorf("tile value is not 2")
+				}
+			}
+		}
+	}
+
+	if startingTiles != 1 {
+		t.Errorf("must start with exactly one tile")
+	}
+}
+
+func TestSeedReproducibility(t *testing.T) {
+	for seed := range 16 {
+		seed := int64(seed)
+		e1 := NewWithSeed(seed)
+		e2 := NewWithSeed(seed)
+
+		for i := range 4 {
+			for j := range 4 {
+				if e1.grid[i][j] != e2.grid[i][j] {
+					t.Errorf("seed %d: grid mismatch at (%d, %d)", seed, i, j)
+
+				}
+			}
+		}
+	}
+}
